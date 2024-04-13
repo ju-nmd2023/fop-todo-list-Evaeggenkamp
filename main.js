6c2d25a3857cc810b0e08e6e9d46fdf6ab12abf2
Evaeggenkamp <Egev23qs@student.ju.se>
2024-04-13 23:14:22 +0200

    lacalstorage, json and deletebutton

--- main.js
+++ main.js
@@ -6,14 +6,60 @@ function updateTime() {
   document.getElementById("time").textContent = timeString;
 
   const options = {
-    weekday: "long",
+    weekday: "short",
     year: "numeric",
-    month: "long",
+    month: "short",
     day: "numeric",
   };
   const dateString = now.toLocaleDateString("numeric", options);
   document.getElementById("date").textContent = dateString;
 }
-setInterval(updateTime, 1000);
+setInterval(updateTime, 60000);
 
 updateTime();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const input = document.getElementById("todo-input");
+  const todoList = document.getElementById("todo-list");
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  function renderTasks() {
+    todoList.innerHTML = "";
+
+    for (let i = tasks.length - 1; i >= 0; i--) {
+      const taskText = tasks[i];
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      const deleteButton = document.createElement("li");
+      deleteButton.textContent = "X";
+      deleteButton.addEventListener("click", function () {
+        tasks.splice(i, 1);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        renderTasks();
+      });
+
+      taskItem.appendChild(deleteButton);
+
+      taskItem.addEventListener("click", function () {
+        this.classList.toggle("completed");
+      });
+      todoList.insertBefore(taskItem, todoList.firstChild);
+    }
+  }
+
+  renderTasks();
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskText = input.value.trim();
+    if (taskText !== "") {
+      tasks.push(taskText);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTasks();
+      input.value = "";
+    }
+  });
+});
